81e9c21f49b4bbd0e1c695cc3943dcfefa5d0d4f
Hemadri92642 <mshemadrireddy@gmail.com>
2025-11-21 10:03:53 +0530

    change the code for jenkins and greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project for devops Technologies master.............";
+        return "DevOps Project for devops Technologies test.............";
     }
 }
